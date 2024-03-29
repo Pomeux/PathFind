3e79e1916c007891e7b51c92f8c3bebd1862db10
Planque Léo <134111837+Pomeux@users.noreply.github.com>
2024-03-29 05:04:20 +0100

    Update Dijkstra.jl

--- src/Dijkstra.jl
+++ src/Dijkstra.jl
@@ -1,6 +1,6 @@
 include("Grille.jl")
 
-#Dijkstra.jl
+#	
 
 #Si poids négatif on peut utiliser Algorithme de Bellmon-Ford pour éviter les boucles
 #Pour implementer l'arbre on le représente sous forme de tableau. Pour éviter de refaire des allocutions on lui donne en taille le nombre de sommet 
@@ -54,7 +54,7 @@ function Dijkstra(g::Grill)
 		for i in voi #max 4 iterations
 			(yV,xV)=i
 
-			if( file[yV,xV]>d+poids(g,(y,x),(yV,xV))) #changer après
+			if( file[yV,xV]>d+poids(g,(yV,xV))) #changer après
 				
 				arbre[yV,xV]=(y,x)	
 				
@@ -82,87 +82,8 @@ function Dijkstra(g::Grill)
 
 end
 #ajouter exception si pas voisins, mettre CN,CT,CP dans la grille et faire exception dans la grille 
-function poids(g::Grill,premier,deuxieme::Tuple{Int64,Int64}) 
-
-	(yD,xD)=(premier[1],premier[2])
-	(yF,xF)=(deuxieme[1],deuxieme[2])
-
-	
 
 
-
-		if(g.tab[yF,xF].etat==N)
-			
-			return 1
-		#=elseif((g.tab[yD,xD].etat==N && g.tab[yF,xF].etat==P) || (g.tab[yD,xD].etat==P && g.tab[yF,xF].etat==N))
-
-			
-			return g.CT=#
-
-		elseif(g.tab[yF,xF].etat==P)
-			#println("b")
-			return 8
-		elseif(g.tab[yF,xF].etat==PP)
-			#println("b")
-			return 5
-
-		end
-	#=else
-		#lever exception
-	end=#
-end
-
-
-
-#=
-debut=(3,2)
-fin=(1,2)
-
-tabb=[
-['p','l','l','l'],
-['p','b','b','l'],
-['l','l','l','l'],
-
-
-]
-=#
-#=
-debut=(1,1)
-fin=(3,3)
-
-tabb=[
-['l','l','l'],
-['l','l','l'],
-['l','l','l'],
-
-
-]
-
-
-tab::Matrix{Case}=Matrix{Case}(undef,3,3)
-
-
-
-for i in 1:3
-	for j in 1:3
-		if(tabb[i][j]=='p')
-			tab[i,j]=Case(P,false,false)
-		elseif(tabb[i][j]=='l')
-			tab[i,j]=Case(N,false,false)
-		elseif(tabb[i][j]=='b')
-
-			tab[i,j]=Case(B,false,false)
-		end
-	end
-end
-
-g=Grill(tab,debut,fin,0,0,1,3,5)
-
-
-	
-Dijkstra(g)
-
-=#
 
 
 
