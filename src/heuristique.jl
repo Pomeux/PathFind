40438e2dde2940c93cb042a1c49530999a4154e7
Planque Léo <134111837+Pomeux@users.noreply.github.com>
2024-03-29 05:06:07 +0100

    Update heuristique.jl

--- src/heuristique.jl
+++ src/heuristique.jl
@@ -10,35 +10,6 @@ mutable struct Noeud
 
 end
 
-function poids(g::Grill,deuxieme::Tuple{Int64,Int64}) 
-
-	(yF,xF)=(deuxieme[1],deuxieme[2])
-
-	
-
-
-
-		if(g.tab[yF,xF].etat==N)
-			
-			return 1
-		#=elseif((g.tab[yD,xD].etat==N && g.tab[yF,xF].etat==P) || (g.tab[yD,xD].etat==P && g.tab[yF,xF].etat==N))
-
-			
-			return g.CT=#
-
-		elseif(g.tab[yF,xF].etat==P)
-			#println("b")
-			return 8
-		elseif(g.tab[yF,xF].etat==PP)
-			#println("b")
-			return 5
-
-		end
-	#=else
-		#lever exception
-	end=#
-end
-
 
 function heursitique(g::Grill)
 
@@ -114,37 +85,3 @@ function manhattan((yD,xD),(yF,xF)::Tuple{Int64,Int64})
 	return abs(xF-xD)+abs(yF-yD)
 end
 
-debut=(3,2)
-fin=(1,2)
-
-tabb=[
-['p','l','l','l'],
-['p','b','b','l'],
-['l','l','l','l'],
-['l','l','l','l']
-
-]
-
-
-
-tab::Matrix{Case}=Matrix{Case}(undef,4,4)
-
-
-
-for i in 1:4
-	for j in 1:4
-		if(tabb[i][j]=='p')
-			tab[i,j]=Case(P,false,false)
-		elseif(tabb[i][j]=='l')
-			tab[i,j]=Case(N,false,false)
-		elseif(tabb[i][j]=='b')
-
-			tab[i,j]=Case(B,false,false)
-		end
-	end
-end
-
-g=Grill(tab,debut,fin,0,0,1,3,5)
-
-
-heursitique(g)
